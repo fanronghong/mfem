29cb97b7e41afd9561a201713d5aa23ebc97a93b
fanronghong <1161799932@qq.com>
2020-05-26 09:38:34 +0800

    add *mesh, *msh into data/

--- miniapps/pnp/examples/example.cpp
+++ miniapps/pnp/examples/example.cpp
@@ -1,5 +1,6 @@
 
 #include <iostream>
+#include <fstream>
 #include "mfem.hpp"
 #include "../utils/SelfDefined_LinearForm.hpp"
 #include "../utils/DGSelfTraceIntegrator.hpp"
@@ -8,6 +9,14 @@ using namespace mfem;
 
 int main()
 {
-    using namespace _DGSelfTraceIntegrator;
-    Test_DGSelfTraceIntegrator();
+//    using namespace _DGSelfTraceIntegrator;
+//    Test_DGSelfTraceIntegrator();
+
+    Mesh mesh(16, 16, 16, Element::TETRAHEDRON, true, 2.0, 2.0, 2.0);
+    {
+        ofstream mesh_file("./16_16_16.mesh");
+        mesh_file.precision(14);
+        mesh.Print(mesh_file);
+    }
+
 }
