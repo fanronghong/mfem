39e60311e47a36e980e9617b093b2e9c4376a7bd
fanronghong <1161799932@qq.com>
2020-05-31 17:38:33 +0800

    c

--- miniapps/pnp/pnp_box/run.sh
+++ miniapps/pnp/pnp_box/run.sh
@@ -6,35 +6,37 @@ reset
 # https://unix.stackexchange.com/questions/145651/using-exec-and-tee-to-redirect-logs-to-stdout-and-a-log-file-in-the-same-time
 exec &> >(tee "./out")
 
-./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des cg
-./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des dg
-./pnp_steadystate_box -p 1 -ref 0 -lin newton -des cg
-./pnp_steadystate_box -p 1 -ref 0 -lin newton -des dg
 
-./pnp_steadystate_box -p 2 -ref 0 -lin gummel -des cg
-./pnp_steadystate_box -p 2 -ref 0 -lin gummel -des dg
-./pnp_steadystate_box -p 2 -ref 0 -lin newton -des cg
-./pnp_steadystate_box -p 2 -ref 0 -lin newton -des dg
+#./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des dg
+#./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des dg
+#./pnp_steadystate_box -p 1 -ref 2 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 2 -lin gummel -des dg
+#./pnp_steadystate_box -p 1 -ref 3 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 3 -lin gummel -des dg
 
-./pnp_steadystate_box -p 3 -ref 0 -lin gummel -des cg
-./pnp_steadystate_box -p 3 -ref 0 -lin gummel -des dg
-./pnp_steadystate_box -p 3 -ref 0 -lin newton -des cg
-./pnp_steadystate_box -p 3 -ref 0 -lin newton -des dg
+#./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 0 -lin gummel -des dg
+#./pnp_steadystate_box -p 2 -ref 0 -lin gummel -des cg
+#./pnp_steadystate_box -p 2 -ref 0 -lin gummel -des dg
+#./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des dg
+#./pnp_steadystate_box -p 2 -ref 1 -lin gummel -des cg
+#./pnp_steadystate_box -p 2 -ref 1 -lin gummel -des dg
+#./pnp_steadystate_box -p 1 -ref 2 -lin gummel -des cg
+#./pnp_steadystate_box -p 1 -ref 2 -lin gummel -des dg
+#./pnp_steadystate_box -p 2 -ref 2 -lin gummel -des cg
+#./pnp_steadystate_box -p 2 -ref 2 -lin gummel -des dg
 
 
+#./pnp_steadystate_box -p 1 -ref 0 -lin newton -des cg
+#./pnp_steadystate_box -p 1 -ref 0 -lin newton -des dg
+#./pnp_steadystate_box -p 1 -ref 1 -lin newton -des cg
+#./pnp_steadystate_box -p 1 -ref 1 -lin newton -des dg
+#./pnp_steadystate_box -p 1 -ref 2 -lin newton -des cg
+#./pnp_steadystate_box -p 1 -ref 2 -lin newton -des dg
+./pnp_steadystate_box -p 1 -ref 3 -lin newton -des cg
+./pnp_steadystate_box -p 1 -ref 3 -lin newton -des dg
 
 
-./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des cg
-./pnp_steadystate_box -p 1 -ref 1 -lin gummel -des dg
-./pnp_steadystate_box -p 1 -ref 1 -lin newton -des cg
-./pnp_steadystate_box -p 1 -ref 1 -lin newton -des dg
-
-./pnp_steadystate_box -p 2 -ref 1 -lin gummel -des cg
-./pnp_steadystate_box -p 2 -ref 1 -lin gummel -des dg
-./pnp_steadystate_box -p 2 -ref 1 -lin newton -des cg
-./pnp_steadystate_box -p 2 -ref 1 -lin newton -des dg
-
-./pnp_steadystate_box -p 3 -ref 1 -lin gummel -des cg
-./pnp_steadystate_box -p 3 -ref 1 -lin gummel -des dg
-./pnp_steadystate_box -p 3 -ref 1 -lin newton -des cg
-./pnp_steadystate_box -p 3 -ref 1 -lin newton -des dg
