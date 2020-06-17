5f3132e1077c1ca00f366db3f5058ea6bdb0a639
fanronghong <1161799932@qq.com>
2020-06-17 18:32:45 +0800

    c

--- miniapps/pnp/pnp_protein/run.sh
+++ miniapps/pnp/pnp_protein/run.sh
@@ -5,7 +5,7 @@ reset
 exec &> >(tee "./out") # https://unix.stackexchange.com/questions/145651/using-exec-and-tee-to-redirect-logs-to-stdout-and-a-log-file-in-the-same-time
 
 # ------>
-./pnp_steadystate_protein -p 1 -lin gummel -dis cg -v -debug
+./pnp_steadystate_protein -p 1 -lin gummel -dis cg
 ./pnp_steadystate_protein -p 1 -lin newton -dis cg
 ./pnp_steadystate_protein -p 1 -lin gummel -dis dg
 
