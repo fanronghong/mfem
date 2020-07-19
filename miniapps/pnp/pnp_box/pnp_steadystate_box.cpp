176284f70e5741da02a08a9452c70eb20c11bb5f
fanronghong <1161799932@qq.com>
2020-07-19 12:11:19 +0800

    c

--- miniapps/pnp/pnp_box/pnp_steadystate_box.cpp
+++ miniapps/pnp/pnp_box/pnp_steadystate_box.cpp
@@ -13,7 +13,6 @@ int main(int argc, char *argv[])
 {
     int num_procs, myid;
     MPI_Init(&argc, &argv);
-    MFEMInitializePetsc(NULL, NULL, options_src, NULL);
     MPI_Comm_size(MPI_COMM_WORLD, &num_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
@@ -34,6 +33,8 @@ int main(int argc, char *argv[])
         MPI_Finalize();
         return 1;
     }
+
+    MFEMInitializePetsc(NULL, NULL, options_src, NULL);
 
     if (ComputeConvergenceRate)
     {
