1ae0c7a64a97ae546379b8686ca7d95b7dbc0a6f
fanronghong <1161799932@qq.com>
2020-07-21 16:25:49 +0800

    c

--- miniapps/pnp/pnp_box/pnp_steadystate_box.hpp
+++ miniapps/pnp/pnp_box/pnp_steadystate_box.hpp
@@ -13,11 +13,11 @@ using namespace mfem;
 
 const char* mesh_file       = "./4_4_4_translate.msh";
 int p_order                 = 1; //有限元基函数的多项式次数
-int refine_times            = 1;
+int refine_times            = 0;
 const char* Linearize       = "newton"; // newton, gummel
 const char* Discretize      = "dg"; // cg, dg
-bool ComputeConvergenceRate = true; // 利用解析解计算误差阶
 const char* options_src     = "./pnp_box_petsc_opts";
+bool ComputeConvergenceRate = true; // 利用解析解计算误差阶
 bool local_conservation     = true;
 
 const int bottom_attr       = 1;
